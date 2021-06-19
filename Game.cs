12e20a1ce9cd0e6fb8026891988780e4a8a375b7
RGBoosted <right0346@gmail.com>
2021-06-19 13:03:16 +0300

    Revert "Added all project"
    This reverts commit b59cd041e06b75fef495cadf9109fac0201a6624.

---
+++ Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTelegramBot
+{
+    public abstract class Game
+    {
+       static string name { get; set; }
+       static int id { get; set; }
+
+
+    }
+}
